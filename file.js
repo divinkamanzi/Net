a17863183c8b4670bc9f71af599b76424e17bc29
KD <138771448+divinkamanzi@users.noreply.github.com>
2023-07-11 23:16:40 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Add click event listener to the subscribe button
+    var subscribeButton = document.querySelector("button");
+    subscribeButton.addEventListener("click", function() {
+      alert("You have subscribed to Netflix!");
+    });
+  });
+  
